58c37beba868dba906fc5328571f4f4f8521801d
scottbooma <scottbooma@gmail.com>
2022-05-19 16:08:29 -0400

    Move data from component to service

--- src/app/pokemon-display/pokemon-display.component.ts
+++ src/app/pokemon-display/pokemon-display.component.ts
@@ -1,36 +1,13 @@
 import { Component } from '@angular/core';
+import { PokemonService, PokemonDetail } from '../pokemon.service';
 
-type Ability = {
-  name: string;
-  description: string;
-};
-
-type PokemonDetail = {
-  name: string;
-  imageUrl: string;
-  abilities: Ability[];
-};
 @Component({
   selector: 'app-pokemon-display',
   templateUrl: './pokemon-display.component.html',
   styleUrls: ['./pokemon-display.component.css'],
 })
 export class PokemonDisplayComponent {
-  pokemon: PokemonDetail = {
-    name: 'Ditto',
-    imageUrl:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png',
-    abilities: [
-      {
-        name: 'Imposter',
-        description: 'Transforms upon entering battle.',
-      },
-      {
-        name: 'Imposter',
-        description: 'Transforms upon entering battle.',
-      },
-    ],
-  };
+  pokemon: PokemonDetail = this.pokemonService.pokemon;
 
-  constructor() {}
+  constructor(private pokemonService: PokemonService) {}
 }
--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -4,10 +4,37 @@ export type Pokemon = {
   name: string;
   imageUrl: string;
 };
+
+export type Ability = {
+  name: string;
+  description: string;
+};
+
+export type PokemonDetail = {
+  name: string;
+  imageUrl: string;
+  abilities: Ability[];
+};
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
+  pokemon: PokemonDetail = {
+    name: 'Ditto',
+    imageUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png',
+    abilities: [
+      {
+        name: 'Imposter',
+        description: 'Transforms upon entering battle.',
+      },
+      {
+        name: 'Imposter',
+        description: 'Transforms upon entering battle.',
+      },
+    ],
+  };
+
   allPokemon: Pokemon[] = [
     {
       name: 'Ditto',
