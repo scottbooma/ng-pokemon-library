a4f3ccbaee6159504b847cbc7660fad58d5640f0
scottbooma <scottbooma@gmail.com>
2022-05-19 15:49:33 -0400

    Add pokemon display component with data hardcoded in template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDisplayComponent } from './pokemon-display/pokemon-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PokemonListComponent
+    PokemonListComponent,
+    PokemonDisplayComponent
   ],
   imports: [
     BrowserModule,
