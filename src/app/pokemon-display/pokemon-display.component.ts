29cb31a8fb8aa8fb0907c048f8af6d98473f49c2
scottbooma <scottbooma@gmail.com>
2022-05-19 16:00:55 -0400

    Move detail data from template to component

--- src/app/pokemon-display/pokemon-display.component.ts
+++ src/app/pokemon-display/pokemon-display.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+type Ability = {
+  name: string;
+  description: string;
+};
+
+type PokemonDetail = {
+  name: string;
+  imageUrl: string;
+  abilities: Ability[];
+};
 @Component({
   selector: 'app-pokemon-display',
   templateUrl: './pokemon-display.component.html',
-  styleUrls: ['./pokemon-display.component.css']
+  styleUrls: ['./pokemon-display.component.css'],
 })
-export class PokemonDisplayComponent implements OnInit {
+export class PokemonDisplayComponent {
+  pokemon: PokemonDetail = {
+    name: 'Ditto',
+    imageUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png',
+    abilities: [
+      {
+        name: 'Imposter',
+        description: 'Transforms upon entering battle.',
+      },
+      {
+        name: 'Imposter',
+        description: 'Transforms upon entering battle.',
+      },
+    ],
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
