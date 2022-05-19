0c4f5e1f051217099e75a919f4aee3724c9bd747
scottbooma <scottbooma@gmail.com>
2022-05-19 16:35:02 -0400

    Add simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonDisplayComponent } from './pokemon-display/pokemon-display.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/pokemon-list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'pokemon-list',
+    component: PokemonListComponent,
+  },
+  {
+    path: 'pokemon-display',
+    component: PokemonDisplayComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
